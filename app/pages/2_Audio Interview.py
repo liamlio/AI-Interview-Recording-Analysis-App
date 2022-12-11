b31c88c6933d38dc1af280f2c7805684cbe7fb2a
Liam <35613341+liamlio@users.noreply.github.com>
2022-12-11 10:20:02 -0500

    Update 2_Audio Interview.py

--- app/pages/2_Audio Interview.py
+++ app/pages/2_Audio Interview.py
@@ -37,6 +37,10 @@ user_name = st.text_input("First and Last name", placeholder="Jane Doe")
 #### IF audio component is what's slowing everything down, remove it and replace with upload file
 #### Then just link users to an online audio recorders to get the files from
 ## ADD INSTRUCTIONS HERE!!!
+st.write("### Instructions")
+st.write("1. For each question below, record your answer using [https://online-voice-recorder.com/](https://online-voice-recorder.com/).")
+st.write("2. Once you're happy with your recording, upload the recording for each respective question below.")
+st.write("3. Click Submit once finished and wait for all the recordings to be uploaded and processed before closing this tab")
 
 st.write("Please limit all responses to less then 3 minutes in audio length.")
 for i, question in enumerate(current_campaign["questions"]):
@@ -48,7 +52,6 @@ for i, question in enumerate(current_campaign["questions"]):
 
 submitted = st.button("Submit Interview")
 if submitted:
-    # prevent submit if a question is None
     custom_models = pd.read_csv(Path(f"app/pages/database/{st.session_state.user_id}/custom_models_{st.session_state.user_id}.csv"), index_col=0)
     custom_models = {model_name:model_desc for model_name, model_desc in custom_models.values}
     st.write("Please don't close your browser yet")
@@ -58,6 +61,9 @@ if submitted:
     Path(user_path).mkdir(parents=True, exist_ok=True)
     for i in range(len(current_campaign["questions"])):
         val = st.session_state[f"question_{i}"]
+        if val is None:
+            st.error(f"Please submit question {i+1}")
+            break
         dest_file = open(user_path / f'question_{i}.mp3', 'wb+')
         dest_file.write(val.getbuffer())
         dest_file.close()
